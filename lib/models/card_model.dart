21664e5c93580151e222e2158127638f23ff0f68
Diego Del Rio <diegockmm@gmail.com>
2026-08-05 21:59:21 -0400

    cambios diego

--- lib/models/card_model.dart
+++ lib/models/card_model.dart
@@ -1,7 +1,4 @@
-enum RouteCashCardType {
-  debit,
-  credit,
-}
+enum RouteCashCardType { debit, credit }
 
 class RouteCashCardModel {
   const RouteCashCardModel({
@@ -12,9 +9,7 @@ class RouteCashCardModel {
     required this.availableAmount,
     required this.type,
     required this.assetPath,
-    this.cardNumber,
     this.expiryDate,
-    this.cvv,
   });
 
   final String id;
@@ -24,7 +19,56 @@ class RouteCashCardModel {
   final double availableAmount;
   final RouteCashCardType type;
   final String assetPath;
-  final String? cardNumber;
   final String? expiryDate;
-  final String? cvv;
+
+  RouteCashCardModel copyWith({
+    String? id,
+    String? bankName,
+    String? productName,
+    String? lastFourDigits,
+    double? availableAmount,
+    RouteCashCardType? type,
+    String? assetPath,
+    String? expiryDate,
+  }) {
+    return RouteCashCardModel(
+      id: id ?? this.id,
+      bankName: bankName ?? this.bankName,
+      productName: productName ?? this.productName,
+      lastFourDigits: lastFourDigits ?? this.lastFourDigits,
+      availableAmount: availableAmount ?? this.availableAmount,
+      type: type ?? this.type,
+      assetPath: assetPath ?? this.assetPath,
+      expiryDate: expiryDate ?? this.expiryDate,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'bank_name': bankName,
+      'product_name': productName,
+      'last_four_digits': lastFourDigits,
+      'available_amount': availableAmount,
+      'type': type.name,
+      'asset_path': assetPath,
+      'expiry_date': expiryDate,
+    };
+  }
+
+  factory RouteCashCardModel.fromJson(Map<String, dynamic> json) {
+    return RouteCashCardModel(
+      id: json['id'] as String,
+      bankName: json['bank_name'] as String,
+      productName: json['product_name'] as String,
+      lastFourDigits: json['last_four_digits'] as String,
+      availableAmount: (json['available_amount'] as num?)?.toDouble() ?? 0,
+      type: json['type'] == RouteCashCardType.credit.name
+          ? RouteCashCardType.credit
+          : RouteCashCardType.debit,
+      assetPath: json['asset_path'] as String? ?? '',
+      expiryDate: json['expiry_date'] as String?,
+    );
+  }
 }
+
